b9f0965c13f2e8aef2f5213a32b9cd1ee06e5802
Jigar Jain <jjain91@gmail.com>
2020-07-21 21:35:53 +0200

    feat: implement producer for stock prices

---
+++ commands/kafka-console-consumer-consume.sh
@@ -0,0 +1,1 @@
+kafka-console-consumer --bootstrap-server=kafka:9092 --topic topstocks
--- src/producers/index.ts
+++ src/producers/index.ts
@@ -1,16 +1,74 @@
 import "../env";
 import kafkaClient from "../kafkaClient";
-import inputData from "./data.json";
+import WebSocket from "ws";
+import topStocks from "./data/top-stocks.json";
+import { Message } from "kafkajs";
 
 (async () => {
+  // Connecting to Kafka producer
   const producer = kafkaClient.producer();
 
   await producer.connect();
 
-  const response = await producer.send({
-    topic: "pokemons",
-    messages: inputData,
+  const ws = new WebSocket(
+    `wss://ws.finnhub.io?token=${process.env.FINNHUB_API_KEY}`
+  );
+
+  // Open WS connection to Finnhub
+  ws.on("open", () => {
+    console.log("Connected to Finnhub");
+
+    topStocks.forEach((stock) => {
+      console.log(
+        `Subscribing to stock prices of ${stock.name} [${stock.symbol}]`
+      );
+      ws.send(
+        JSON.stringify({
+          type: "subscribe",
+          symbol: stock.symbol,
+        })
+      );
+    });
   });
 
-  console.log(response);
+  ws.on("message", async (incomingMessage) => {
+    try {
+      const { data } = JSON.parse(String(incomingMessage));
+
+      if (!data || !data.length) {
+        return;
+      }
+
+      const messages: Message[] = [];
+      type IncomingStockPrice = {
+        s: string;
+        p: number;
+      };
+
+      data.forEach((d: IncomingStockPrice) => {
+        const message = {
+          key: d.s,
+          value: JSON.stringify({
+            symbol: d.s,
+            price: d.p,
+          }),
+        };
+
+        messages.push(message);
+      });
+
+      const topic = process.env.KAFKA_TOPIC_NAME_TOPSTOCKS;
+
+      if (topic) {
+        const response = await producer.send({
+          topic: topic,
+          messages: messages,
+        });
+      } else {
+        console.error("Env value for `KAFKA_TOPIC_NAME_STOCKS` not set");
+      }
+    } catch (e) {
+      console.error("Error while processing message", e);
+    }
+  });
 })();
